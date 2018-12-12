da7e9833f75585e316c2b8f092b56c932947ba9f
Daffanie Hurley <daffhur50@gmail.com>
2018-12-12 11:23:45 -0600

    Added the ability to specify vhosts and service directives.

--- re.sh
+++ re.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+CONFIG="$1"
+COMMAND="$2"
+
+if [ $# -ne 2 ]
+then 
+    echo "$0 requires two parameters {virtual-host} {restart|reload}"
+    exit 1
+fi
+
+
 #Move the current execution state to the proper directory
 cd /etc/apache2/sites-available
 
